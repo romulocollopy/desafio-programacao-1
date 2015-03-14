90dffb4baa11425effee37c7fb4c606e42ca9ff3
Rômulo Collopy <romulocollopy@gmail.com>
2015-03-14 00:44:28 -0300

    Configura app para Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn purchases.wsgi
--- purchases/settings.py
+++ purchases/settings.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os, sys
+
+import dj_database_url
+
 from purchases.tests import DisableMigrations
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -61,11 +64,10 @@ WSGI_APPLICATION = 'purchases.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+        )
     }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -85,6 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 if 'test' in sys.argv[1:]:
